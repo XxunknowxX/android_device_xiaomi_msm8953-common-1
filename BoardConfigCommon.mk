amarbajpai/device_xiaomi_msm8953-common

msm8953-common: Import and adapt keydisabler from LOS tree

BoardConfigCommon.mk
@@ -163,6 +163,10 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
+# Key disabler
+JAVA_SOURCE_OVERLAYS := \
+    org.pixelexperience.keydisabler|$(DEVICE_PATH)/keydisabler|**/*.java
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 

XxunknowxX/android_device_xiaomi_msm8953-common-1

BoardConfigCommon.mk
@@ -174,6 +174,10 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
+# Key disabler
+JAVA_SOURCE_OVERLAYS := \
+    org.pixelexperience.keydisabler|$(VENDOR_PATH)/keydisabler|**/*.java
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 